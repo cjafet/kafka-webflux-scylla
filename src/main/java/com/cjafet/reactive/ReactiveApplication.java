11884dc9e127157ec14066c0ba073aa882bf80e3
Carlos Jafet Neto <cjafet07@gmail.com>
2019-07-08 15:10:07 -0300

    Add bean for kafka configuration

--- src/main/java/com/cjafet/reactive/ReactiveApplication.java
+++ src/main/java/com/cjafet/reactive/ReactiveApplication.java
@@ -1,13 +1,19 @@
 package com.cjafet.reactive;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.kafka.core.KafkaAdmin;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class ReactiveApplication {
@@ -21,5 +27,24 @@ public class ReactiveApplication {
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
         EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.H2).build();
         return db;
+    }
+
+    @Value(value = "${kafka.server}")
+    private String bootstrapServer;
+
+    @Value(value = "${kafka.topic}")
+    private String kafkaTopic;
+
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic1() {
+        return new NewTopic(kafkaTopic, 1, (short) 1);
     }
 }
